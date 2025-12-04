4b2862e439f1b382bd4a7c1c5f90a8826be40767
abhinav <mishraabhinav7054@gmail.com>
2025-12-04 16:57:06 +0530

    install talwind properlly

--- src/App.js
+++ src/App.js
@@ -3,12 +3,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header header-bg text red flex-column justify-center align-center p-20 fs-24 fw-600 ">
-        <p>
-          Jai Bhole Studio
-        </p>
-      </header>
-    </div>
+      <h1>Welcome to Jai Bhole Studio</h1>
+      <p>Your one-stop destination for creative design and art.</p>
+     
+        </div>
   );
 }
 
