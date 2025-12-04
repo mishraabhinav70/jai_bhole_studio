67f95b37278533ae6fcf58a0a1d27583f597f6ec
abhinav <mishraabhinav7054@gmail.com>
2025-12-04 16:42:18 +0530

    demo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header header-bg text red flex-column justify-center align-center p-20 fs-24 fw-600 ">
         <p>
           Jai Bhole Studio
         </p>
